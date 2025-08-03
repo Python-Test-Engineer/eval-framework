2eeae64fc04b0c108bdfb2ac9db8989fdb33d10d
Craig West <iwswordpress@gmail.com>
2025-08-03 13:49:29 +0100

    add GROQ

--- src/ragas/udemy/test_1.py
+++ src/ragas/udemy/test_1.py
@@ -1,4 +1,4 @@
-# Pytest 
+# Pytest
 import os
 
 import pytest
@@ -7,13 +7,29 @@ from langchain_openai import ChatOpenAI
 from ragas import SingleTurnSample
 from ragas.llms import LangchainLLMWrapper
 from ragas.metrics import LLMContextPrecisionWithoutReference
+from langchain_groq import ChatGroq
 from dotenv import load_dotenv, find_dotenv
+from rich.console import Console
+
+console = Console()
 
 load_dotenv(find_dotenv(), override=True)
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+GROQ_API_KEY = os.environ["GROQ_API_KEY"]
 MODEL = "gpt-4o-mini"
 
 print("Starting...")
+
+
+# Initialize Groq LLM
+# llm = ChatGroq(
+#     model="llama-3.3-70b-versatile",  # or other Groq models
+#     groq_api_key=GROQ_API_KEY,  # or set GROQ_API_KEY env var
+#     temperature=0.0,
+# )
+# # # Use it like any LangChain LLM
+# response = llm.invoke("What is the capital of France?")
+# console.print(f"[green]GROQ TEST: {response.content}[/]")
 
 
 @pytest.mark.asyncio
@@ -21,7 +37,12 @@ async def test_context_precision():
     # create object of class for that specific metric
 
     # power of LLM + method metric ->score
-    llm = ChatOpenAI(model=MODEL, temperature=0)
+    # llm = ChatOpenAI(model=MODEL, temperature=0)
+    llm = ChatGroq(
+        model="llama-3.3-70b-versatile",  # or other Groq models
+        groq_api_key=GROQ_API_KEY,  # or set GROQ_API_KEY env var
+        temperature=0.0,
+    )
     langchain_llm = LangchainLLMWrapper(llm)
     context_precision = LLMContextPrecisionWithoutReference(llm=langchain_llm)
     question = "How many articles are there in the Selenium webdriver python course?"
@@ -45,5 +66,5 @@ async def test_context_precision():
     # score
     score = await context_precision.single_turn_ascore(sample)
     print("SCORE: ", score)
-  
+
     assert score > 0.8
