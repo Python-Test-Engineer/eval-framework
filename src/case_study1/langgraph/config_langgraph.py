92f218444f662b53c45596f1442676ab2a073868
Craig West <iwswordpress@gmail.com>
2025-09-01 15:06:14 +0100

    rename

--- src/case_study1/langgraph/config_langgraph.py
+++
@@ -1,8 +0,0 @@
-# Config values for Langgraph app_article_writer
-
-PROVIDER = "openai"
-MODEL = "gpt-4o-mini"
-TEMPERATURE = 0
-LANGUAGE = "FRENCH"
-SUBJECT = "AI, ML, Data Science, Programming, Web, Technology"
-CONTENT_LENGTH = 100
